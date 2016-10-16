acacd2c7801af7fe958be42191d1cd4eb618d7a6
Mikl <Mikl@DESKTOP-RA8NT1N>
2016-10-16 16:23:24 +0300

    1.10

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -58,6 +58,10 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -79,7 +83,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(2, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(219, 285);
+            this.pictureBox1.Size = new System.Drawing.Size(247, 315);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -113,56 +117,67 @@
             this.Column8});
             this.dataGridView1.Location = new System.Drawing.Point(255, 118);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.dataGridView1.Size = new System.Drawing.Size(203, 179);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
             // 
             // Column1
             // 
             this.Column1.HeaderText = "1";
             this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
             this.Column1.Width = 25;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "2";
             this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             this.Column2.Width = 25;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "3";
             this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
             this.Column3.Width = 25;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "4";
             this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
             this.Column4.Width = 25;
             // 
             // Column5
             // 
             this.Column5.HeaderText = "5";
             this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
             this.Column5.Width = 25;
             // 
             // Column6
             // 
             this.Column6.HeaderText = "6";
             this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
             this.Column6.Width = 25;
             // 
             // Column7
             // 
             this.Column7.HeaderText = "7";
             this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
             this.Column7.Width = 25;
             // 
             // Column8
             // 
             this.Column8.HeaderText = "8";
             this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
             this.Column8.Width = 25;
             // 
             // dataGridView2
@@ -180,9 +195,12 @@
             this.dataGridViewTextBoxColumn8});
             this.dataGridView2.Location = new System.Drawing.Point(495, 118);
             this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.dataGridView2.Size = new System.Drawing.Size(203, 179);
             this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -255,7 +273,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(422, 12);
+            this.button3.Location = new System.Drawing.Point(401, 10);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(140, 37);
             this.button3.TabIndex = 7;
@@ -266,46 +284,92 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(422, 53);
+            this.button4.Location = new System.Drawing.Point(401, 53);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(140, 37);
             this.button4.TabIndex = 8;
             this.button4.Text = "Save rates";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.Location = new System.Drawing.Point(585, 12);
+            this.button5.Location = new System.Drawing.Point(558, 12);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(140, 37);
             this.button5.TabIndex = 9;
             this.button5.Text = "Inverse fast Walsh transform";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button6.Location = new System.Drawing.Point(585, 53);
+            this.button6.Location = new System.Drawing.Point(558, 53);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(140, 37);
             this.button6.TabIndex = 10;
             this.button6.Text = "Save byte array of picture";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(731, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(704, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(219, 285);
+            this.pictureBox2.Size = new System.Drawing.Size(247, 315);
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(255, 303);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(149, 24);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Save first basis";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(495, 303);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(149, 24);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Save second basis";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(650, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(959, 339);
+            this.ClientSize = new System.Drawing.Size(959, 389);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -361,6 +425,10 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -14,15 +14,55 @@ namespace WindowsFormsApplication1
         int width;
         int height;
         byte[,,] byteOfPicture;
+
         public Form1()
         {
             InitializeComponent();
             dataGridView1.RowCount = 8; dataGridView2.RowCount = 8;
-            //dataGridView1.AutoSize = true;
 
+            label3.Text = "Det= 0";
+            label4.Text = "Det= 0";
 
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = "0";
+                }
+            }
 
+            for (int i = 0; i < 8; i++) dataGridView1.Rows[0].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 1; i < 8; i++) dataGridView1.Rows[1].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 2; i < 8; i++) dataGridView1.Rows[2].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 3; i < 8; i++) dataGridView1.Rows[3].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 4; i < 8; i++) dataGridView1.Rows[4].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 5; i < 8; i++) dataGridView1.Rows[5].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 6; i < 8; i++) dataGridView1.Rows[6].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 7; i < 8; i++) dataGridView1.Rows[7].Cells[7 - i].Style.BackColor = Color.DarkGray;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = "0";
+                }
+            }
+
+            for (int i = 0; i < 8; i++) dataGridView2.Rows[0].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 1; i < 8; i++) dataGridView2.Rows[1].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 2; i < 8; i++) dataGridView2.Rows[2].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 3; i < 8; i++) dataGridView2.Rows[3].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 4; i < 8; i++) dataGridView2.Rows[4].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 5; i < 8; i++) dataGridView2.Rows[5].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 6; i < 8; i++) dataGridView2.Rows[6].Cells[7 - i].Style.BackColor = Color.DarkGray;
+            for (int i = 7; i < 8; i++) dataGridView2.Rows[7].Cells[7 - i].Style.BackColor = Color.DarkGray;
         }
+
+
+
+
+
+
 
         //пытался писать понятный код//
         public static void BitmapToByteRgb(ref Bitmap bmp, ref byte[,,] Grafikbyte)
@@ -109,7 +149,7 @@ namespace WindowsFormsApplication1
             return WHT_Masiv;
         }//обратоное преобразование уолша
 
-        public static void swapRowsColumns <T>(ref T[] rgbImage, int width)
+        public static void swapRowsColumns<T>(ref T[] rgbImage, int width)
         {
             for (int chanel = 0; chanel < 3; chanel++)
             {
@@ -126,36 +166,96 @@ namespace WindowsFormsApplication1
             }
         }//Разворот масива
 
-       // public static void GenereteBasisOfWals(ref int[,] IndexMatrix, ref int[,] BasisOfWalsh)
-       // {
-       //     BasisOfWalsh = new int[256, 256];
-       //     for (int i = 0; i < 8; i++)
-       //         for (int j = 0; j < 8; j++)
-       //         {
-       //             BasisOfWalsh[2 ^ i, 2 ^ j] = IndexMatrix[7 - i, j];//запонение 2^n-го числа 
-       //         }
-       //     for(int j = 1; j < 256; j++)
-       //     for (int i = 1; i < 256; i++)
-       //     {
-       //         if (BasisOfWalsh[j, i] == null)
-       //         {
-       //             for (int k = 128; k > 1; k = k / 2)
-       //             {
-       //                 int m = i;
-       //                 if (k < m)
-       //                 {
-       //                     BasisOfWalsh[j, i] += BasisOfWalsh[j, k];
-       //                 }
-       //             }
-       //         }
-       //     }
-       //}//Создание базиса на основе индексной матрици
-                
+        public static void GenereteBasisOfWals(ref int[,] IndexMatrix, ref int[,] BasisOfWalsh)
+        {
+            BasisOfWalsh = new int[256, 256];
+            for (int a = 0; a < 256; a++)
+                for (int b = 0; b < 256; b++)
+                {
+                    BasisOfWalsh[a, b] = -1;
+                }
 
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    BasisOfWalsh[2 ^ i, 2 ^ j] = IndexMatrix[7 - i, j];//запонение 2^n-го числа 
+                }
+            for (int j = 1; j < 256; j++)
+                for (int i = 1; i < 256; i++)
+                {
+                    if (BasisOfWalsh[j, i] == -1)
+                    {
+                        int n = i;
+                        BasisOfWalsh[j, i] = 0;
+                        for (int k = 128; k >= 1; k = k / 2)
+                        {
+                            if (k <= n)
+                            {
+                                BasisOfWalsh[j, i] += BasisOfWalsh[j, k];
+                                n = n - k;
+                            }
+                        }
+                    }
+                }
+            for (int j = 1; j < 256; j++)
+                for (int i = 1; i < 256; i++)
+                {
+                    if (BasisOfWalsh[i, j] == -1)
+                    {
+                        int n = j;
+                        BasisOfWalsh[i, j] = 0;
+                        for (int k = 128; k >= 1; k = k / 2)
+                        {
+                            if (k <= n)
+                            {
+                                BasisOfWalsh[i, j] += BasisOfWalsh[i, k];
+                                n = n - k;
+                            }
+                        }
+                    }
+                }
+            for (int i = 0; i < 256; i++)
+            {
+                BasisOfWalsh[0, i] = 0;
+                BasisOfWalsh[i, 0] = 0;
+            }
+        }//Создание базиса на основе индексной матрици
 
+        public static double[,] GetMinor(double[,] matrix, int row, int column)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new Exception(" Число строк в матрице не совпадает с числом столбцов");
+            double[,] buf = new double[matrix.GetLength(0) - 1, matrix.GetLength(0) - 1];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if ((i != row) || (j != column))
+                    {
+                        if (i > row && j < column) buf[i - 1, j] = matrix[i, j];
+                        if (i < row && j > column) buf[i, j - 1] = matrix[i, j];
+                        if (i > row && j > column) buf[i - 1, j - 1] = matrix[i, j];
+                        if (i < row && j < column) buf[i, j] = matrix[i, j];
+                    }
+                }
+            return buf;
+        }
+  
+        public static double Determ(double[,] matrix)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new Exception(" Число строк в матрице не совпадает с числом столбцов");
+            double det = 0;
+            int Rank = matrix.GetLength(0);
+            if (Rank == 1) det = matrix[0, 0];
+            if (Rank == 2) det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            if (Rank > 2)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    det += Math.Pow(-1, 0 + j) * matrix[0, j] * Determ(GetMinor(matrix, 0, j));
+                }
+            }
+            return det;
+        }
 
-
-       
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -172,21 +272,23 @@ namespace WindowsFormsApplication1
                 pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             }
 
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             byte[] Bufer = new byte[3 * width * height];
             ThreeToOne(ref byteOfPicture, ref width, ref height, ref Bufer);
+
             string[] S = new string[3 * width * height];
-            
+
             for (int i = 0; i < Bufer.Length; i++)
             {
                 S[i] = Convert.ToString(Bufer[i]);
             }
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                System.IO.File.WriteAllLines(saveFileDialog1.FileName+".txt", S);             
+                System.IO.File.WriteAllLines(saveFileDialog1.FileName + ".txt", S);
             }
             Array.Clear(S, 0, S.Length);
             Array.Clear(Bufer, 0, Bufer.Length);
@@ -196,7 +298,7 @@ namespace WindowsFormsApplication1
         {
             byte[] bufer = new byte[3 * width * height];
             ThreeToOne(ref byteOfPicture, ref width, ref height, ref bufer);
-           double[] MasivRates = new double [3 * width * height];
+            double[] MasivRates = new double[3 * width * height];
 
 
             int[,] firstMatrix = new int[8, 8];
@@ -208,10 +310,128 @@ namespace WindowsFormsApplication1
                     firstMatrix[i, j] = Convert.ToInt16(dataGridView1.Rows[i].Cells[j].Value);
 
                 }
+            GenereteBasisOfWals(ref firstMatrix, ref firstBasis);
+            FastWalsTransform<double>(ref MasivRates, ref firstBasis);
+        }
 
-         
+        private void button4_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int[,] FirsrIndexMatrix = new int[8, 8];
+            int[,] FirstBasisOfWalsh = new int[256, 256];
+            string[] bufer = new string[256 * 256];
+
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    FirsrIndexMatrix[i, j] = Convert.ToInt16(dataGridView1.Rows[i].Cells[j].Value);
+                }
+            GenereteBasisOfWals(ref FirsrIndexMatrix, ref FirstBasisOfWalsh);
+
+            for (int i = 0; i < 256; i++)
+                for (int j = 0; j < 256; j++)
+                {
+                    bufer[i + 256 * j] = Convert.ToString(FirstBasisOfWalsh[i, j]);
+                }
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllLines(saveFileDialog1.FileName + ".txt", bufer);
+            }
+
+        }
+
+        
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            double[,] Matrix = new double[8, 8];
+
+            if ((e.RowIndex == 0 && e.ColumnIndex < 8) || (e.RowIndex == 1 && e.ColumnIndex < 7) || (e.RowIndex == 2 && e.ColumnIndex < 6) || (e.RowIndex == 3 && e.ColumnIndex < 5) || (e.RowIndex == 4 && e.ColumnIndex < 4) || (e.RowIndex == 5 && e.ColumnIndex < 3) || (e.RowIndex == 6 && e.ColumnIndex < 2) || (e.RowIndex == 7 && e.ColumnIndex < 1))
+            {
+                if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value) == "0")
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 1;
+                    dataGridView1.Rows[7 - e.ColumnIndex].Cells[7 - e.RowIndex].Value = 1;
+                    for (int i = 0; i < 8; i++)
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Matrix[i, j] = Convert.ToDouble(dataGridView1.Rows[j].Cells[i].Value);
+                        }
+                    double det;
+                    det = Determ(Matrix);
+                    label3.Text = "Det= " +Convert.ToString(det);
+                }
+                else
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 0;
+                    dataGridView1.Rows[7 - e.ColumnIndex].Cells[7 - e.RowIndex].Value = 0;
+                    for (int i = 0; i < 8; i++)
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Matrix[i, j] = Convert.ToDouble(dataGridView1.Rows[j].Cells[i].Value);
+                        }
+                    double det;
+                    det = Determ(Matrix);
+                    label3.Text = "Det= " + Convert.ToString(det);
+                }
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            double[,] Matrix = new double[8, 8];
+
+            if ((e.RowIndex == 0 && e.ColumnIndex < 8) || (e.RowIndex == 1 && e.ColumnIndex < 7) || (e.RowIndex == 2 && e.ColumnIndex < 6) || (e.RowIndex == 3 && e.ColumnIndex < 5) || (e.RowIndex == 4 && e.ColumnIndex < 4) || (e.RowIndex == 5 && e.ColumnIndex < 3) || (e.RowIndex == 6 && e.ColumnIndex < 2) || (e.RowIndex == 7 && e.ColumnIndex < 1))
+            {
+                if (Convert.ToString(dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Value) == "0")
+                {
+                    dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 1;
+                    dataGridView2.Rows[7 - e.ColumnIndex].Cells[7 - e.RowIndex].Value = 1;
+                    for (int i = 0; i < 8; i++)
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Matrix[i, j] = Convert.ToDouble(dataGridView2.Rows[j].Cells[i].Value);
+                        }
+                    double det;
+                    det = Determ(Matrix);
+                    label4.Text = "Det= " + Convert.ToString(det);
+                }
+
+                else
+                {
+                    dataGridView2.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 0;
+                    dataGridView2.Rows[7 - e.ColumnIndex].Cells[7 - e.RowIndex].Value = 0;
+                    for (int i = 0; i < 8; i++)
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Matrix[i, j] = Convert.ToDouble(dataGridView1.Rows[j].Cells[i].Value);
+                        }
+                    double det;
+                    det = Determ(Matrix);
+                    label4.Text = "Det= " + Convert.ToString(det);
+
+
+                }
+
+            }
         }
     }
 }
+
 
